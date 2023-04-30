70f02318b0c45ee33482b757cff0c8a108fe824e
kilgette <kilgette@gmail.com>
2023-04-30 14:57:46 -0500

    pre tutor commit

--- script.js
+++ script.js
@@ -6,31 +6,37 @@ $(document).ready(function() {
 
 // function to check the current time and update the background color of each time block based on the status of past, present or future
 function updateTimeBlocks () {
-
+  const currentHour = dayjs().hour();
 }
 
-
-
-
-
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+//function to update the time blocks every hour so that the current time is reflected
+setInterval(updateTimeBlocks, 60 * 60 * 1000); //understand this code! 
+{
 
 });
+
+//loop through each time block 
+
+//call a function to update the background colors of the time blocks depending on the current time 
+
+
+//event click listener for save button
+
+
+ 
+
+
+
+// GIVEN I am using a daily planner to create a schedule
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar
+// WHEN I scroll down
+// THEN I am presented with timeblocks for standard business hours
+// WHEN I view the timeblocks for that day
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+// WHEN I click into a timeblock
+// THEN I can enter an event
+// WHEN I click the save button for that timeblock
+// THEN the text for that event is saved in local storage
+// WHEN I refresh the page
+// THEN the saved events persist
