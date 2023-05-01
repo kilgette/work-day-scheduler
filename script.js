cec5865f1eb7c483dfc4d3359880bfd7cfd1938f
kilgette <kilgette@gmail.com>
2023-04-30 20:18:54 -0500

    cleaned up comments

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
 $(document).ready(function() {
 $(".saveBtn").on("click", function (){
 var description9 = $(".description").val();
@@ -24,7 +22,7 @@ localStorage.setItem("14", description14);
 var description15 = $(".description15").val();
 localStorage.setItem("15", description15);
 
-var description16 = $(".description11").val();
+var description16 = $(".description16").val();
 localStorage.setItem("16", description16);
 
 
@@ -34,28 +32,6 @@ localStorage.setItem("16", description16);
 $(".description").val(localStorage.getItem("9"));
 $(".sky").val(localStorage.getItem("10"));
 $(".description11").val(localStorage.getItem("11"));
-// $(".description").val(localStorage.getItem("9"));
-// function updateTimeBlocks () {
-//   const currentHour = dayjs().hour();
-//   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY") );
@@ -67,7 +43,7 @@ function updateTimeBlocks () {
 }
 
 //function to update the time blocks every hour so that the current time is reflected
-setInterval(updateTimeBlocks, 60 * 60 * 1000); //understand this code! 
+setInterval(updateTimeBlocks, 60 * 60 * 1000); 
 {
 
 };
@@ -75,13 +51,6 @@ setInterval(updateTimeBlocks, 60 * 60 * 1000); //understand this code!
 //loop through each time block 
 
 //call a function to update the background colors of the time blocks depending on the current time 
-
-
-//event click listener for save button
-
-
- 
-
 
 
 // GIVEN I am using a daily planner to create a schedule
