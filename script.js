44cbca7fe074da9a82fabd4078bef1baae7328aa
kilgette <kilgette@gmail.com>
2023-05-04 21:39:23 -0500

    updated color functionality

--- script.js
+++ script.js
@@ -1,4 +1,42 @@
-$(document).ready(function() {
+$(document).ready(function() { 
+
+const currentHour = dayjs();
+
+let currentTime = dayjs().hour()
+
+const timeBlock = document.getElementById('currentDay')
+
+timeBlock.textContent = currentHour
+
+setInterval(() => {
+let presentDateTime = dayjs().format(`dddd, MMMM D, YYYY, HH:mm:ss
+`)
+document.getElementById('currentDay').innerText=presentDateTime
+}, 1000);  
+
+$('.time-block').each(function(){
+  let blockHour = parseInt($(this).attr(`id`).split(`-`)[1])
+
+
+if (currentTime>blockHour) { 
+  $(this).addClass(`past`)
+  console.log(currentTime,blockHour)
+  
+} else if (currentTime===blockHour) {
+  $(this).removeClass(`past`)
+  $(this).addClass(`present`)
+  console.log(currentTime,blockHour)
+
+} else {
+$(this).removeClass(`past`)
+$(this).removeClass(`present`)
+$(this).addClass(`future`)
+console.log(currentTime,blockHour)
+ }
+})
+console.log(currentTime,blockHour)
+
+
 $(".saveBtn").on("click", function (){
 
 var description9 = $(".description9").val();
@@ -29,12 +67,15 @@ localStorage.setItem("16", description16);
 
 // function to check the current time and update the background color of each time block based on the status of past, present or future
 function updateTimeBlocks () {
-  const currentHour = dayjs().hour();
+  const currentTime = dayjs().hour();
 }
+// //selects all the time block divs?
+// const hourID = document.QuerySelectorAll('.hour');
 
-
+//manages current time display 
 $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY") );
 });
+
 
 // function to check the current time and update the background color of each time block based on the status of past, present or future
 function updateTimeBlocks () {
